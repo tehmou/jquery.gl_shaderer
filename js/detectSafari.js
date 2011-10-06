d2ac2257acd212a5e7954909988c15e252eafe24
Timo Tuominen <timo.tuominen@gmail.com>
2011-10-06 18:29:33 +0200

    Added simple.html example and made it possible to only provide urls to textures or shaders.

--- js/detectSafari.js
+++
@@ -1,9 +0,0 @@
-if (navigator.userAgent.indexOf("Safari") !== -1) {
-    try {
-        if (!document.createElement('canvas').getContext('experimental-webgl')) {
-            $("#safari-warning").css("display", "block");
-        }
-    } catch (e) {
-        $("#safari-warning").css("display", "block");
-    }
-}
